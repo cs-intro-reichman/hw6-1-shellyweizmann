c15246d6ac438cfe18c01b1651a546fbace19e6c
Shelly Weizmann <shellyweizmann@Shellys-MacBook-Pro.local>
2025-12-21 21:20:46 +0200

    test 2 Completed assignment

--- Editor2.java
+++ Editor2.java
@@ -10,8 +10,7 @@ import java.awt.Color;
 public class Editor2 {
 
 	public static void main (String[] args){
-		//// Replace this comment with your code.
-		//// This function is similar to the main function of Editor1.java	
+	
 		String fileName = args[0];
         int width  = Integer.parseInt(args[1]);
         int height = Integer.parseInt(args[2]);
